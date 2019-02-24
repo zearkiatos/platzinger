baac1a30a40dd427b7e854c612a146f5bcccc21c
Pedro <caprilespe@outlook.com>
2019-02-24 20:58:46 -0300

    Data type use.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,26 @@ import { LoginPage } from '../login/login';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    console.log("Ok");
+    let c:number = 1;
+    let b:number = 2;
+    let e:string = "1";
+    let f:string = "2";
+    let g:boolean = true;
+    let h:object = {};
+    console.log(g);
+    console.log(h);
+
+    let i = [c,b,e,f,g,h];
+
+    console.log(i);
+
+    let j: boolean [] = [true,g];
+
+    console.log(j);
+
+    let k: object[] = [{},h];
+
+    let l: any[] = [1,'string',{},[]];
   }
 
   goToConversation(){
