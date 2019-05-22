a77b14eb1b3dc9dee27afd0f1bd11b098cd9bf93
Pedro Capriles <caprilespe@outlook.com>
2019-05-21 23:16:43 -0400

    Creando pagina de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ import { AboutPage } from '../pages/about/about';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = ProfilePage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -28,11 +28,11 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'Login', component: LoginPage },
-      { title: 'Conversation', component: ConversationPage },
-      { title: 'Profile', component: ProfilePage },
-      { title: 'About', component: AboutPage }
+      // { title: 'List', component: ListPage },
+      // { title: 'Login', component: LoginPage },
+      // { title: 'Conversation', component: ConversationPage },
+      { title: 'Profile', component: ProfilePage }
+      // { title: 'About', component: AboutPage }
     ];
 
   }
