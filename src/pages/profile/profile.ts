f4c8c3c93678245dfe9c7d124b502b5bbd8c7242
Pedro Capriles <caprilespe@outlook.com>
2019-05-22 23:57:30 -0400

    Agregando editar datos basicos del perfil de usuario

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,9 @@
+import { User } from './../../models/user';
+import { UserService } from './../../services/user.service';
+import { AuthenticationService } from './../../services/authentication.service';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, Button } from 'ionic-angular';
+import { IUser } from '../../app/interfaces/IUser';
 
 /**
  * Generated class for the ProfilePage page.
@@ -11,14 +15,47 @@ import { NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
+  providers:[AuthenticationService, UserService]
 })
 export class ProfilePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public user:User;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthenticationService, private userService: UserService, private alertCtrl:AlertController) {
+    this.authService.getStatus().subscribe((data)=>{
+      this.userService.getUserById(data.uid).valueChanges().subscribe((user:any)=>{
+        this.user = user;
+        console.log(this.user);
+      }, 
+      (error)=>{
+        console.log(error);
+      });
+    },
+    (error)=>{
+      console.log(error);
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+  }
+
+  saveData(){
+
+    this.userService.editUser(this.user).then((data)=>{
+      let alert = this.alertCtrl.create({
+        title:"Mensaje",
+        message:"Datos Actualizados con éxito.",
+        buttons:["Aceptar"]  
+      });
+      alert.present();
+    }).catch((error)=>{
+      let alertError = this.alertCtrl.create({
+        title:"Mensaje",
+        message:"Ocurrio un error al editar los datos. Error: "+error,
+        buttons:["Aceptar"]  
+      });
+      alertError.present();
+      console.log(error);
+    });
   }
 
 }
